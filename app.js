1f12adfaa3c55e35bd716332ae276793196f433b
famthegeek <famthegeek95@gmail.com>
2014-02-28 05:47:30 -0500

    Started on the main features

--- app.js
+++ app.js
@@ -30,7 +30,12 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
+app.get('/', function(req,res){
+	
+	res.render('index', { user: req.user });
+
+});
+
 app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
